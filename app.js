bf3464ccc48df199788b5e95926c3438112c81f2
Devin M <75272179+mDevinn@users.noreply.github.com>
2022-08-29 22:12:13 -0400

    More updates

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function hamburgerMenu() {
     var x = document.getElementById("hamburgerMenu");
+    var y = document.getElementById("nav-links");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
