38707016a02630d90bc722f68c36d27bedbe20d6
devinmerch <devinmerch@gmail.com>
2024-04-06 17:46:57 -0400

    updating dev portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      textColor: {
+        'color-main': '#00143d',
+      },
+      letterSpacing: {
+        wider: '0.1em',
+      },
+      fontFamily: {
+        'space': ['Space Grotesk', 'sans-serif'],
+      },
+      backgroundColor: { 
+        'main': '#00143d',
+        'secondary': '#fca791', 
+      },
+      borderColor: { 
+        'main': '#00143d', 
+      },
+    }
+  },
+  plugins: [],
+}
+
